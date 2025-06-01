9eee708a4bd82cbfd50db3415f5ff7ecb186dd08
Veda Dasara <vedashree.d1997@gmail.com>
2025-06-01 11:14:41 -0400

    Updated config files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/portfolio/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './index.html', 
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
